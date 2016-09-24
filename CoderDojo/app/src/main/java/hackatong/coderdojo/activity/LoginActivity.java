4173fba37da93378256b70eec52015fee52f518a
riojano0 <danielelnuevo@hotmail.com>
2016-09-24 15:43:35 -0300

    commit

--- CoderDojo/app/src/main/java/hackatong/coderdojo/activity/LoginActivity.java
+++ CoderDojo/app/src/main/java/hackatong/coderdojo/activity/LoginActivity.java
@@ -1,15 +1,15 @@
 package hackatong.coderdojo.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-import hackatong.coderdojo.R;
-
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+
 }
